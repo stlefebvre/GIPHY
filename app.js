543a79554ddeabb312888d8f595dcc30670ce31b
stlefebvre <lefebvre.shelby@gmail.com>
2018-04-19 18:39:05 -0400

    created API URL variables, and API keys

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ $( document ).ready(function() {
 
     var topics = ["Rottweiler", "Bulldog", "Boston Terrier", "Golden Retriever", "Newfoundland", "Labradoodle", "Great Dane", "Bloodhound"]
 
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + dogbreed + "&api_key=QiYJDZ83GQwGYuz6IraIqLK988IyQNEC"
+        //Look back at homework for q, limit, and rating
 
-
+    var dogbreed = $("#breed-input-field").val()
 });
