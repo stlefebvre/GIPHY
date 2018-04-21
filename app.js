eebdfb3849fc2a76219db383f73fd4aaf1e913eb
stlefebvre <lefebvre.shelby@gmail.com>
2018-04-21 00:01:46 -0400

    Got AJAX call working correctly

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-$( document ).ready(function() {
+$(document).ready(function () {
+    $("#search").on("click", function(event) {
+        console.log("clicked");
+        // var topics = ["Rottweiler", "Bulldog", "Boston Terrier", "Golden Retriever", "Newfoundland", "Labradoodle", "Great Dane", "Bloodhound"];
+        var dogbreed = $("#breed-input-field").val().trim();
 
-    var topics = ["Rottweiler", "Bulldog", "Boston Terrier", "Golden Retriever", "Newfoundland", "Labradoodle", "Great Dane", "Bloodhound"]
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=ArrnUSeTmBQ1nq8tmaRirhx1HWuu8GrK&q=" + dogbreed + "&limit=10&offset=0&rating=G&lang=en";
 
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + dogbreed + "&api_key=QiYJDZ83GQwGYuz6IraIqLK988IyQNEC"
-        //Look back at homework for q, limit, and rating
-
-    var dogbreed = $("#breed-input-field").val()
-});
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).done(function (response) {
+            console.log(response)
+        });
+    });
+})
