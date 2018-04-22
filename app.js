24558c1fc3678d185c952baf1bdd2a7e03a2a7c5
stlefebvre <lefebvre.shelby@gmail.com>
2018-04-21 21:14:02 -0400

    got dynamic element working

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 $(document).ready(function () {
+    //Dog breeds variable
+    var topics = ["Rottweiler", "Bulldog", "Boston Terrier", "Golden Retriever", "Newfoundland", "Labradoodle", "Great Dane", "Bloodhound"];
+
+    //Function to create buttons from topics array
+    function topicsButtons () {
+        //empties div so there are no duplicate buttons
+        $("#buttons-go-here").empty();
+        for (var i = 0; i < topics.length; i++) {
+            var a = $("<button>");
+            a.addClass("dogbreed");
+            a.attr("data-name", topics[i]);
+            a.text(topics[i]);
+            $("#buttons-go-here").append(a)
+        }
+    }
+
     $("#search").on("click", function(event) {
         console.log("clicked");
-        // var topics = ["Rottweiler", "Bulldog", "Boston Terrier", "Golden Retriever", "Newfoundland", "Labradoodle", "Great Dane", "Bloodhound"];
-        var dogbreed = $("#breed-input-field").val().trim();
+        event.preventDefault();
 
-        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=ArrnUSeTmBQ1nq8tmaRirhx1HWuu8GrK&q=" + dogbreed + "&limit=10&offset=0&rating=G&lang=en";
+        var dogbreeds = $("#breed-input-field").val().trim();
+
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=ArrnUSeTmBQ1nq8tmaRirhx1HWuu8GrK&q=" + dogbreeds + "&limit=10&offset=0&rating=G&lang=en";
 
         $.ajax({
             url: queryURL,
@@ -12,5 +29,10 @@ $(document).ready(function () {
         }).done(function (response) {
             console.log(response)
         });
+
+        topics.push(dogbreeds);
+
+        topicsButtons();
+
     });
 })
